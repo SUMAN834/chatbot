95bf30396a50a283fd139af786bef971724fd516
SUMAN834 <83573183+SUMAN834@users.noreply.github.com>
2023-09-25 11:55:29 +0530

    Add files via upload

---
+++ chatbot/responses.js
@@ -0,0 +1,13 @@
+const responses = {
+    'hi': "Hello! How can I help you?",
+    'how are you': "I'm Lolita, but I'm functioning well!",
+    'account issues': "I'm sorry to hear you're facing account issues. Please report your queries using our report form, and our bank employee will contact you for assistance.",
+    'what loans are available': "We offer several types of loans at our bank:\n1. Home Loan\n2. Business Loan\n3. Education Loan\n\nIs there a specific type of loan you would like to know more about?",
+    'home loan': "Our Home Loan program helps you finance the purchase of your dream home. You can get more details and apply on our website.",
+    'business loan': "Our Business Loan program is designed to support entrepreneurs and businesses. You can get more details and apply on our website.",
+    'education loan': "Our Education Loan program can help you fund your educational expenses. You can get more details and apply on our website.",
+    'low interest services': "We are proud to offer low-interest services to our valued customers. Whether you're looking for loans, savings accounts, or investment opportunities, we strive to provide competitive interest rates to help you achieve your financial goals. For more information about our low-interest services, please visit our website or contact our customer support team.",
+    'default': "I'm sorry, I couldn't understand your question. For more information, please contact our higher officials."
+};
+
+// You don't need to export the responses object in this case
